5889e4ae58e98f1f88cf890c577ad31a8bb7d9f9
lucifier <asshekhar206@gmail.com.com>
2018-01-04 05:04:48 +0530

    slide prev next button

--- resources/views/layouts/slide.blade.php
+++ resources/views/layouts/slide.blade.php
@@ -21,6 +21,8 @@
 		    <button>Photographer</button>
 		  </div>
 		</div>
+		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+		<a class="next" onclick="plusSlides(1)">&#10095;</a>
 	</div>
 	  
 @endsection
